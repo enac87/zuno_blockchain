1e509a432bfea36f3174c74245d4a4cc2c987e52
Alain Emaga <alaincesar@hotmail.fr>
2020-05-18 22:33:04 +0100

    Working on Blockchain constructor and methods

---
+++ dev/test.js
@@ -0,0 +1,20 @@
+function Blockchain() {
+  this.chain = [];
+  this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transaction: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
